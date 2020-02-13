ce525da76295af036075e9a443dd1fdb5a9b7277
Eduardo Gusmão Filho <42840856+EduardoBBGusmao@users.noreply.github.com>
2020-02-13 13:53:16 -0300

    Add files via upload

---
+++ precache-manifest.72713494ebc5cfe4f1f2aef9c918e14e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a779368a938bc6274c962521872ac38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4547dee3ed86eae2472c",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "03043f4685c3e62ebf12",
+    "url": "/static/css/main.1095d915.chunk.css"
+  },
+  {
+    "revision": "4547dee3ed86eae2472c",
+    "url": "/static/js/2.efca385a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.efca385a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03043f4685c3e62ebf12",
+    "url": "/static/js/main.28c50a3b.chunk.js"
+  },
+  {
+    "revision": "2989595f2546bfb0b1db",
+    "url": "/static/js/runtime-main.60ebcab2.js"
+  },
+  {
+    "revision": "cef63d6377dfe52a8d779a3dd4c45fb6",
+    "url": "/static/media/LemonMilk.cef63d63.otf"
+  },
+  {
+    "revision": "bb255cd0eddfad3e65855d0128dce38e",
+    "url": "/static/media/LemonMilkbold.bb255cd0.otf"
+  },
+  {
+    "revision": "36589485139932b7d331879a37d21f24",
+    "url": "/static/media/bg.36589485.jpg"
+  },
+  {
+    "revision": "c0b8d3ad8c293eec7068915c59cc4f00",
+    "url": "/static/media/logo-final.c0b8d3ad.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
